778166ee25065726d5ce55a2467a654aa31667c8
Yana <yanaegorova69.3@gmail.com>
2021-08-29 03:52:20 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,142 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Bath Cover Safety Guard',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Duck-shaped spout guard covers faucet to help prevent bumps and burns.',
+            },
+            {
+                text: 'Fits most spout types and works with shower diverters.',
+            },
+            {
+                text: 'Built-in bubble bath dispenser.',
+            },
+            {
+                text: 'Spout guard measures 7.5"in x 4"in (top opening measures 4"in x 1"in).',
+            },
+            {
+                text: 'Made of durable material and no paint, which means you do not need to worry about chipping!',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.jpeg'],
+    },
+    {
+        id: 'product-2',
+        name: '2 Water Bottles',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img2.png',
+        description: [
+            {
+                text: '25-ounce water bottle (2-pack) with detachable infuser; ideal for home, school, office, and outdoor adventures.',
+            },
+            {
+                text: 'Transparent bottle made with shatterproof, BPA-free, non-toxic, co-polyester plastic for reliable, long-lasting strength.',
+            },
+            {
+                text: 'Secure-fitting blue top with a wide spout and a flip-top cap that opens easily and snaps into place (won’t swing around when drinking).',
+            },
+            {
+                text: 'Dishwasher-safe (top rack) for easy cleaning; not suitable for hot beverages; do not microwave.',
+            },
+            {
+                text: 'Measures 3 by 3 by 9.1 inches.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png' , './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Portable Butane Stove',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Instastart ignition for matchless lighting; 7,650 total BTUs of cooking power.',
+            },
+            {
+                text: 'Fits up to a 10-inch pan.',
+            },
+            {
+                text: 'Lasts up to 1.25 hours on high on one 8.8 oz. butane gas cylinder (sold separately).',
+            },
+            {
+                text: 'Adjustable burner gives precise temperature control, and large base offers stability for easy stirring.',
+            },
+            {
+                text: 'Durable porcelain-coated grate is easy to clean, and aluminum burner is rust-proof for years of use.',
+            },
+            {
+                text: 'Carry case included.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Sleep Foam Pillow',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Our supportive three-layer foam pillow helps align your neck and keeps you cool all night.',
+            },
+            {
+                text: 'A meticulously designed inner layer contours to your head and neck in all sleep positions, while two plush outer layers offer cushioning comfort.',
+            },
+            {
+                text: 'Tiny channels in the foam circulate air out and away to prevent overheating. Our extra breathable jersey knit cover looks cool and sleeps even cooler.',
+            },
+            {
+                text: 'Made from materials that are safe for you and the planet.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.jpeg', './img/img4-6.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Yarn Ball Winder',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'This package includes one yarn ball winder: winder body, cone, yarn guide, yarn guide mount, 2 swing arms and instructions.',
+            },
+            {
+                text: 'Conveniently clamps on to a table.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Automatic Cat Puppy Feeder',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Feed from your phone,your cat will not meow at 3am anymore (2.4GHz WIFI, pls.).',
+            },
+            {
+                text: 'Personalized Pet Feeding: Meals can be dispensed from 5g to 50g.Set meal plan, feed anytime.',
+            },
+            {
+                text: 'Duo Fresh Lock System: Keep the food dry and fresh.',
+            },
+            {
+                text: 'Weight Sensor: You’ll get notices when food bucket in low condition,and an alert when empty.',
+            },
+            {
+                text: 'Energy Backup: this pet feeder will be operated with optional battery backup',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+    },
+]
