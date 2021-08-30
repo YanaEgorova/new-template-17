3b067acb704c6956f5a4c755f4c9db5d652adc19
Yana <yanaegorova69.3@gmail.com>
2021-08-30 20:38:03 +0300

    +

--- js/data/products.js
+++ js/data/products.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: 'product-1',
         name: 'Bath Cover Safety Guard',
-        price: 6.96,
+        price: 19.99,
         image: './img/img1.png',
         description: [
             {
@@ -26,8 +26,8 @@ export const products = [
     {
         id: 'product-2',
         name: '2 Water Bottles',
-        price: 29.99,
-        discount: 24.65,
+        price: 24.99,
+        discount: 14.99,
         image: './img/img2.png',
         description: [
             {
@@ -50,9 +50,98 @@ export const products = [
     },
     {
         id: 'product-3',
-        name: 'Portable Butane Stove',
-        price: 64.90,
+        name: 'Pet Kennel',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img7.png',
+        description: [
+            {
+                title: 'Pet crate for small dogs',
+                text: 'This small dog kennel has a top entry door so you can access your cat or dog. Suitable for small dog breeds between 10-20 lbs. Measures: 24” long and 15” high.',
+            },
+            {
+                title: 'Air travel approved',
+                text: 'This pet carrier meets most airline cargo specifications. The air travel kennel for dogs & cats also has a durable, ergonomic comfort-grip handle & ventilated sides for comfortable transport.',
+            },
+            {
+                title: 'Kennels & houses',
+                text: 'Crate & kennel training is vital for dog safety & comfort. We provide traditional travel carriers, wire training & exercise kennels, play pens, barn-style dog houses & more.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png','./img/img7-4.png', './img/img7-5.png', './img/img7-6.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Rechargeable LED Neck Light',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img8.png',
+        description: [
+            {
+                title: 'Hands free neck light',
+                text: 'This rugged & water resistant light leaves your hands free to work. It rests on your neck without leaving sweat marks & features rechargeable batteries with 9 hours of run time per light. USB cord included.',
+            },
+            {
+                title: 'Special features',
+                text: 'This adjustable neck light features 4 modes, from 15 up to 300 lumens. The 2 dual articulating beam heads can be adjusted & locked independently up to 90 degrees for a wider light.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Travel Makeup Case',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Premium material',
+                text: 'This Makeup Bag is made of high quality Oxford Fabric, Lining-nylon. (Note: Cosmetic Accessories Not included).',
+            },
+            {
+                title: 'Plenty of storage space',
+                text: 'This Makeup Bag have enough space to storage your makeup and cosmetic accessories, like lipstick, lip gloss, makeup brushes, eyeshadow, makeup palettes, hairbrushes, skin care products, nail polish, nail art tools, shampoo and so no.',
+            },
+            {
+                title: 'Adjustable compartments',
+                text: 'This Makeup Bag have several compartments and makeup brushes slots, you can keep your makeup tools is neat and tidy. Special design adjustable compartments, you can adjust as needed.',
+            },
+            {
+                title: 'Perfect travel makeup case',
+                text: 'Portable and Lightweight design, with waterproof, shockproof, anti-wear and spill-proof interiors. You can bring your cosmetics to anywhere.',
+            },
+            {
+                title: 'Multifunctional makeup bag',
+                text: 'This Makeup Bag not only can storage your cosmetics, but also Jewelry, Electronic Accessories, Camera, Essential oil, Toiletries, Shaving Kit, Valuable objects and so on.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.jpeg', './img/img9-3.jpeg', './img/img9-5.png', './img/img9-6.png', './img/img9-7.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Heart Rate Monitor',
+        price: 49.95,
         discount: 69.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Simply remove the heart rate module, and it’s washable, too.',
+            },
+            {
+                text: 'Don’t worry about running out of juice.',
+            },
+            {
+                text: 'The battery lasts for up to 3.5 years.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Portable Butane Stove',
+        price: 69.99,
+        discount: 69.94,
         image: './img/img3.png',
         description: [
             {
@@ -77,10 +166,32 @@ export const products = [
         images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png'],
     },
     {
-        id: 'product-4',
+        id: 'product-8',
+        name: 'Baseball Batting Tee',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Our tee is the original Pro style batting Tee, with the original rolled rubber top.',
+            },
+            {
+                text: 'Utilized by more travel ball, college and professional players than any other batting Tee.',
+            },
+            {
+                text: 'Our patented, hand-rolled flexible rubber ball rest allows hitters to feel the ball not the Tee at contact.',
+            },
+            {
+                text: 'As shown, Tee the original adjustable 26-43 inches.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png', './img/img11-4.png', './img/img11-5.png', './img/img11-6.png', './img/img11-7.png'],
+    },
+    {
+        id: 'product-9',
         name: 'Sleep Foam Pillow',
-        price: 98.90,
-        discount: 55.97,
+        price: 84.41,
+        discount: 95.55,
         image: './img/img4.png',
         description: [
             {
@@ -99,10 +210,36 @@ export const products = [
         images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.jpeg', './img/img4-6.png'],
     },
     {
-        id: 'product-5',
+        id: 'product-10',
+        name: 'Handheld Fabric Steamer',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img12.png',
+        description: [
+            {
+                title: 'Handheld Steamer',
+                text: 'This steamer features 1250 watts of power and provides up to 15 minutes of continuous, deep penetrating steam; kills 99.9 percent of bacteria as well as dust mites and bed bugs.',
+            },
+            {
+                title: 'Anti Calcification Filter',
+                text: 'Features 2 steam settings, an anti calcification filter to reduce calcium buildup for better long term performance, an easy to fill removeable 7.8 ounce water tank and 9 foot power cord.',
+            },
+            {
+                title: '3 in 1 Attachments',
+                text: 'Includes a Silicone band to pull fabric taut for better results, a Bonnet to help protect delicate fabrics.',
+            },
+            {
+                title: 'Fabric Steamers',
+                text: 'Our garment steamers have earned the Good Housekeeping Seal and keep your clothing, upholstery, curtains and other home fabrics smooth and fresh. Check out entire line including handheld, upright and travel steamers.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.png', './img/img12-4.png'],
+    },
+    {
+        id: 'product-11',
         name: 'Yarn Ball Winder',
         price: 119.98,
-        discount: 79.93,
+        discount: 110.89,
         image: './img/img5.png',
         description: [
             {
@@ -115,10 +252,10 @@ export const products = [
         images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
     },
     {
-        id: 'product-6',
+        id: 'product-12',
         name: 'Automatic Cat Puppy Feeder',
         price: 148.00,
-        discount: 95.50,
+        discount: 120.88,
         image: './img/img6.png',
         description: [
             {
